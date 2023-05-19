70b0e13ac7a860384e3afc823bc1c26c44071efa
Anshu Pandey <anshuspandey@gmail.com>
2023-05-19 12:35:01 +0530

    made changes

--- src/model/train.py
+++ src/model/train.py
@@ -5,23 +5,25 @@ import glob
 import os
 
 import pandas as pd
-
+import mlflow
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.sklearn.autolog()
 
     # read data
-    df = get_csvs_df(args.training_data)
+    df = spark.sql("select * from diabetes;").toPandas()
 
     # split data
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model.predict(X_test)
 
 
 def get_csvs_df(path):
@@ -34,11 +36,21 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+               'TricepsThickness', 'SerumInsulin', 'BMI',
+               'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.30,
+                                                        random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=1 / reg_rate,
+                               solver="liblinear").fit(X_train, y_train)
+    return model
 
 
 def parse_args():
@@ -46,8 +58,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
+
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.01)
 
@@ -56,6 +67,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
